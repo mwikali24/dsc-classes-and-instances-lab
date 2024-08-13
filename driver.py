ac82140d726d94435af6ae8099a0e83d7f8355ac
“Lilian_Muli” <mulililian2017@gmail.com>
2024-08-14 00:24:29 +0300

    initial_commit

--- driver.py
+++ driver.py
@@ -1,2 +1,12 @@
 class Driver:
     pass
+class Passenger:
+    pass
+class Ride:
+    pass
+meryl = Passenger()
+daniel = Passenger()
+flatiron_taxi = Driver()
+ride_to_school = Ride()
+ride_home = Ride()
+
